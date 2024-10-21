be8589caa6fa56ee587403abb540a1d547239dad
MarT14X <158273196+MarT14X@users.noreply.github.com>
2024-10-21 21:06:48 +0300

    Delete M1XT.Studio-main directory

--- M1XT.Studio-main/Scripts/App.js
+++
@@ -1,8 +0,0 @@
-const checks = document.getElementsByClassName
-('menu_section');
-
-for (i = 0; i<checks.length; i++) {
-checks[i].addEventListener('click', function () {
-    this.classList.toggle('open')
-})
-}
